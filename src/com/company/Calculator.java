19284d59e9fde7eebe17bd3e9b390ee44cdca601
CodeGangsta44 <dovorman44@gmail.com>
2019-10-24 21:10:02 +0300

    Created template

---
+++ src/com/company/Calculator.java
@@ -0,0 +1,8 @@
+package com.company;
+
+interface Calculator {
+    int add(int a, int b);
+    int diff(int a, int b);
+    int multi(int a, int b);
+    int div(int a, int b);
+}
---
+++ src/com/company/CalculatorImpl.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class CalculatorImpl implements Calculator {
+    @Override
+    public int add(int a, int b) {
+        return 0;
+    }
+
+    @Override
+    public int diff(int a, int b) {
+        return 0;
+    }
+
+    @Override
+    public int multi(int a, int b) {
+        return 0;
+    }
+
+    @Override
+    public int div(int a, int b) {
+        return 0;
+    }
+}

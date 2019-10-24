503d4029f4abcd6a869b7f18a0559b975306dd31
CodeGangsta44 <dovorman44@gmail.com>
2019-10-24 22:03:15 +0300

    Added method impl

--- src/main/java/com/company/CalculatorImpl.java
+++ src/main/java/com/company/CalculatorImpl.java
@@ -3,18 +3,18 @@ package com.company;
 public class CalculatorImpl implements Calculator {
 
     public int add(int a, int b) {
-        return 0;
+        return a + b;
     }
 
     public int diff(int a, int b) {
-        return 0;
+        return a - b;
     }
 
     public int multi(int a, int b) {
-        return 0;
+        return a * b;
     }
 
     public int div(int a, int b) {
-        return 0;
+        return a / b;
     }
 }
